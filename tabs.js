ea306284170a168b881c7d138560ab5ba9187fb2
JayP-TA <jptorressnl19@gmail.com>
2021-11-01 12:10:30 -0700

    Adds tab navigation to destination page

--- tabs.js
+++ tabs.js
@@ -1,6 +1,11 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+})
 
 
 let tabFocus= 0;
@@ -10,19 +15,17 @@ function changeTabFocus (e) {
 
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
 
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
-    }
-
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus <= -1) {
-            tabFocus = tabs.length - 1;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus <= -1) {
+                tabFocus = tabs.length - 1;
+            }
         }
     }
 
@@ -30,3 +33,28 @@ function changeTabFocus (e) {
     tabs[tabFocus].focus();
 }
 
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    const previousTab = tabContainer.querySelector('[aria-selected="true"]');
+
+    hideContent(mainContainer, '[role="tabpanel"]');
+    hideContent(mainContainer, 'picture');
+    previousTab.setAttribute("aria-selected", false);
+    showContent(mainContainer, [`#${targetPanel}`])
+    showContent(mainContainer, [`#${targetImage}`]);
+    targetTab.setAttribute("aria-selected", true);
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.hidden = true);
+}
+
+function showContent(parent, content) {
+     parent.querySelector(content).hidden = false;
+}
